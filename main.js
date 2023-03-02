f3ce35eb7908d3bec25e385bb4dea31490425f7b
wandakim <nwd1583@naver.com>
2023-03-02 21:53:34 +0900

    JS: Navbar menu click & scroll

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 'use strict';
 
+// navbar beautiful
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
   if (navbarHeight < window.scrollY) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
-  console.log(navbarHeight);
-  console.log(window.scrollY);
+});
+
+// scroll into view
+// 클릭되면 해당 아이템으로 scroll into view
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const link = event.target.dataset.link;
+  if (!link) {
+    return;
+  }
+  console.log(link);
+  const element = document.querySelector(`${link}`);
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
 });
